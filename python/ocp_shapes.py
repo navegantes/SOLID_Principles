57fedb324e219d872bb58df7dd6eb5456fe1c52c
navegantes <raphaelnavegantes@gmail.com>
2024-05-13 15:51:48 -0300

    update python files

--- python/ocp_shapes.py
+++ python/ocp_shapes.py
@@ -2,20 +2,20 @@ from abc import ABC, abstractmethod
 from math import pi
 
 # Bad example
-# class Shape:
-#     def __init__(self, shape_type, **kwargs):
-#         self.shape_type = shape_type
-#         if self.shape_type == "rectangle":
-#             self.width = kwargs["width"]
-#             self.height = kwargs["height"]
-#         elif self.shape_type == "circle":
-#             self.radius = kwargs["radius"]
+class ShapeBad:
+    def __init__(self, shape_type, **kwargs):
+        self.shape_type = shape_type
+        if self.shape_type == "rectangle":
+            self.width = kwargs["width"]
+            self.height = kwargs["height"]
+        elif self.shape_type == "circle":
+            self.radius = kwargs["radius"]
 
-#     def calculate_area(self):
-#         if self.shape_type == "rectangle":
-#             return self.width * self.height
-#         elif self.shape_type == "circle":
-#             return pi * self.radius**2
+    def calculate_area(self):
+        if self.shape_type == "rectangle":
+            return self.width * self.height
+        elif self.shape_type == "circle":
+            return pi * self.radius**2
 
 
 # Good example
--- python/srp_file_manager.py
+++ python/srp_file_manager.py
@@ -2,23 +2,23 @@ from pathlib import Path
 from zipfile import ZipFile
 
 # Bad example
-# class FileManager:
-#     def __init__(self, filename):
-#         self.path = Path(filename)
+class FileManagerBad:
+    def __init__(self, filename):
+        self.path = Path(filename)
 
-#     def read(self, encoding="utf-8"):
-#         return self.path.read_text(encoding)
+    def read(self, encoding="utf-8"):
+        return self.path.read_text(encoding)
 
-#     def write(self, data, encoding="utf-8"):
-#         self.path.write_text(data, encoding)
+    def write(self, data, encoding="utf-8"):
+        self.path.write_text(data, encoding)
 
-#     def compress(self):
-#         with ZipFile(self.path.with_suffix(".zip"), mode="w") as archive:
-#             archive.write(self.path)
+    def compress(self):
+        with ZipFile(self.path.with_suffix(".zip"), mode="w") as archive:
+            archive.write(self.path)
 
-#     def decompress(self):
-#         with ZipFile(self.path.with_suffix(".zip"), mode="r") as archive:
-#             archive.extractall()
+    def decompress(self):
+        with ZipFile(self.path.with_suffix(".zip"), mode="r") as archive:
+            archive.extractall()
 
 
 # Good example
